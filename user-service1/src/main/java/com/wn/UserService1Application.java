dad23b85a0de4a06b4f15e59e14d9ba745b65217
zrf6 <371118344@qq.com>
2019-10-25 17:13:08 +0800

    更新

--- user-service1/src/main/java/com/wn/UserService1Application.java
+++ user-service1/src/main/java/com/wn/UserService1Application.java
@@ -3,12 +3,13 @@ package com.wn;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @MapperScan("com.wn.mapper")
 @ImportResource("classpath:transaction.xml")
 @EnableRedisHttpSession
